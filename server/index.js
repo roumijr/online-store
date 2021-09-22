afbf905834d6d98f1c3c1f9a911b1a0f4477dcc3
Mikhail Roughut'ko <pulsemrboy@gmail.com>
2021-09-22 06:07:16 +0300

    add paging

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const fileupload = require('express-fileupload')
 require('dotenv').config();
 
 const sequelize = require('./db');
@@ -8,13 +10,15 @@ const router = require('./routes/index.js');
 const errorHandler = require('./middleware/ErorrHandlingMiddleware');
 
 
+
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router);
-
+app.use(fileupload({}));
 //middleware должен быть последним в списке 
 app.use(errorHandler);
 
